04f7dd80aca21f73c1bdbbdb01cd1098c3721669
oladapzy <dadesunloro@yahoo.com>
2020-02-19 12:08:53 -0600

    Basic python Numbers for beginners

---
+++ index.py
@@ -0,0 +1,64 @@
+# Learning Python for beginners.
+# Working with Numbers basic
+print(2+2)
+print(1+2+3+4+5-6+6*7*9)
+print(5 * 4 / 2)
+print(109 - 46 + 878 / 9)
+# float always return decimal example
+print(8/5)
+# float can be converted to integer by using floor division (//) which eliminate the decimal point
+print(8//5)
+print(120//23)
+# % operator return the remainder of a division
+print(5 % 4)
+# convert a integer to float
+print(float(45))
+
+# when working with complex numbers always remember your BODMAS
+
+num = (3 * (8 / 2) + 5 - 3) / 4
+# step 1 (3 * 4 + 5 - 3) / 4
+# step 2 (12 +5 - 3) / 4
+# step 3 (17 - 3 )/ 4
+# step 4 15/4
+print(num)
+
+# Square or power maths
+
+print(2**2)
+print(4**3)
+
+# simple rectangle calculation using assignment = and variable
+
+width = 30
+length = 60
+rectangle = width * length
+print(rectangle)
+
+# simple square calculation using assignment = and variable
+
+one_side = 16
+square_result = one_side ** 2
+print(square_result)
+
+# simple project with numbers
+
+bank_amt = 534
+
+hair_product = 20 + 10
+lunch = 9.99
+gas = 30.78
+ice_cream = 7.9
+tax_on_all = 12.56
+
+bank_amt = bank_amt - (hair_product + hair_product + lunch + gas + ice_cream + tax_on_all)
+print(bank_amt)
+
+print(type(45))
+print(type(4.5))
+print(type(4 + 67j))
+
+
+
+
+
